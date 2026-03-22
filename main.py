a46757c6654cfc05d056ce013ba38c6132fc8611
agcerutti <acerutti@edenor.com>
2026-03-22 16:51:34 -0300

    funcionando con multi-threads

--- main.py
+++ main.py
@@ -1,17 +1,45 @@
 import sys
 import os
+import resources_rc
+
 from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog
+from PyQt5.QtCore import QThread, pyqtSignal, QObject
+from PyQt5.QtGui import QIcon
 from PyQt5 import uic
 from datetime import datetime
 
 from core.processor import procesar_imagenes
+
+'''
+para compilar iconos: python -m PyQt5.pyrcc_main resources.qrc -o resources_rc.py
+'''
+
+class Worker(QObject):
+    finished = pyqtSignal()
+    progress = pyqtSignal(int)
+    log = pyqtSignal(str)
+
+    def __init__(self, carpeta_origen, carpeta_destino):
+        super().__init__()
+        self.carpeta_origen = carpeta_origen
+        self.carpeta_destino = carpeta_destino
+
+    def run(self):
+        procesar_imagenes(
+            self.carpeta_origen,
+            self.carpeta_destino,
+            log_callback=self.log.emit,
+            progress_callback=self.progress.emit
+        )
+        self.finished.emit()
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         # Carga de la grafica
         uic.loadUi("main.ui", self)
-
+        self.btnSeleccionar.setIcon(QIcon(":/icons/folder.png"))
+        self.btnProcesar.setIcon(QIcon(":/icons/play.png"))
         self.carpeta = None
 
         # Eventos
@@ -28,37 +56,40 @@ class MainWindow(QMainWindow):
             self.txtLog.append(f"\n 📂 Carpeta seleccionada:\n{carpeta}")
 
     def procesar(self):
-        '''Realiza el cambio de parametros informando tanto en log como en la progress bar'''
-        # Si no hay carpeta definida, da aviso
         if not self.carpeta:
             self.txtLog.append("⚠️ Seleccioná una carpeta primero")
             return
-        # Crea una carpeta donde se colocaran las imagenes procesadas
+
         carpeta_salida = os.path.join(self.carpeta, "output")
-
-        def log(msg):
-            '''log de eventos con timestamp'''
-            ahora = datetime.now().strftime("%H:%M:%S")
-            self.txtLog.append(f"[{ahora}] {msg}")
-            self.txtLog.ensureCursorVisible()
-            QApplication.processEvents()
-
-        def progress(val):
-            '''progreso de la barra de progreso'''
-            self.progressBar.setValue(val)
-            QApplication.processEvents()
-
-        log("🚀 Iniciando procesamiento...")
         self.progressBar.setValue(0)
+        # Crear thread y worker
+        self.thread = QThread()
+        self.worker = Worker(self.carpeta, carpeta_salida)
+        self.worker.moveToThread(self.thread)
+        # Conexiones
+        self.thread.started.connect(self.worker.run)
+        self.worker.finished.connect(self.thread.quit)
+        self.worker.finished.connect(self.worker.deleteLater)
+        self.thread.finished.connect(self.thread.deleteLater)
+        self.worker.progress.connect(self.actualizar_progreso)
+        self.worker.log.connect(self.agregar_log)
+        self.worker.finished.connect(self.proceso_terminado)
+        # UI estado
+        self.btnProcesar.setEnabled(False)
+        self.agregar_log("🚀 Iniciando procesamiento...")
+        self.thread.start()
 
-        procesar_imagenes(
-            self.carpeta,
-            carpeta_salida,
-            log_callback=log,
-            progress_callback=progress
-        )
+    def actualizar_progreso(self, valor):
+        self.progressBar.setValue(valor)
 
-        log("🎉 Proceso finalizado")
+    def agregar_log(self, msg):
+        ahora = datetime.now().strftime("%H:%M:%S")
+        self.txtLog.append(f"[{ahora}] {msg}")
+        self.txtLog.ensureCursorVisible()
+
+    def proceso_terminado(self):
+        self.agregar_log("🎉 Proceso finalizado")
+        self.btnProcesar.setEnabled(True)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
